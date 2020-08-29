96cc905d6b291f26647299ba6feb948f0bd37cde
anjie87 <anjali.sharma87@outlook.com>
2020-08-29 14:04:43 -0500

    Update 9

---
+++ src/main/java/SilkCentralMaven/SilkCentralMaven/Main1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package SilkCentralMaven.SilkCentralMaven;
+
+/**
+ * @author anjal
+ *
+ */
+public class Main1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("Main 1 -- do somthing then call Maven");
+SilkCentralMaven.SCRunExecution(45,"http://desktop-5ivatg8:19120", "327981e9-d9f0-4e00-9a78-1bcb86078ec2");
+
+	}
+
+}
---
+++ src/main/java/SilkCentralMaven/SilkCentralMaven/Main2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package SilkCentralMaven.SilkCentralMaven;
+
+/**
+ * @author anjal
+ *
+ */
+public class Main2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("Main 2 -- do somthing then call Maven");
+SilkCentralMaven.SCRunExecution(1, "", "");
+
+	}
+
+}
