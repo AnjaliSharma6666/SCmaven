b98d165751b9739071a6bc39cae3562797640686
anjie87 <anjali.sharma87@outlook.com>
2020-08-29 06:27:49 -0500

    commit all

---
+++ src/main/java/SilkCentralMaven/SilkCentralMaven/SilkCentralMaven.java
@@ -0,0 +1,48 @@
+package SilkCentralMaven.SilkCentralMaven;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SilkCentralMaven {
+	
+	int SCExecutionNodeId;
+	String SCHost;
+	String SCToken;
+	
+	public SilkCentralMaven(int SC_ExecutionNodeId, String SC_Host, String SC_Token) {
+		this.SCExecutionNodeId = SC_ExecutionNodeId;
+		this.SCHost = SC_Host;
+		this.SCToken = SC_Token;	
+	}
+	
+	 public static void SCRunExecution(int SC_ExecutionNodeId, String SC_Host, String SC_Token) {
+        try {
+        	SilkCentralMaven SCObj = new SilkCentralMaven(SC_ExecutionNodeId, SC_Token, SC_Token);
+        	URL url = new URL( SCObj.SCHost + "/Services1.0/execution/executionplanruns?nodeId=" + SC_ExecutionNodeId);
+        	
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept", "application/json;charset=UTF-8");
+            conn.setRequestProperty("SC-SESSION-ID", SC_Token);
+            conn.setRequestProperty("Content-Type", "application/json");
+            
+			
+			  if (conn.getResponseCode() != 200) { throw new
+			  RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode()); }
+			 
+            
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output;
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+            System.out.println("Exception:- " + e);
+        }
+    }
+}
